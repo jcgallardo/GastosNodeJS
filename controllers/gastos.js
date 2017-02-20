b9aa49e66e19c1d1ed4c15c8b7e8d386d3e54cdd
jcgallardo <jcgallardomorales@gmail.com>
2017-02-20 22:08:02 +0100

    Modal emergente tras introducir un gasto (falta crear la vista completa del gasto)

--- controllers/gastos.js
+++ controllers/gastos.js
@@ -1,29 +1,38 @@
-var mongoose = require('mongoose');
-var Gasto  = mongoose.model('Gasto');
+let mongoose = require('mongoose');
+let Gasto  = mongoose.model('Gasto');
 
 //GET - Return all gastos in the DB
 exports.findAllGastos = function(req, res) {
     Gasto.find(function(err, gastos) {
         if(err) res.send(500, err.message);
 
-        console.log('GET /gasto')
+        console.log('GET /gasto');
             res.status(200).jsonp(gastos);
     });
 };
 exports.addGasto = function(req, res) {
     console.log('POST');
-    //console.log(req.body);
-    //console.log(req.session);
-    var gasto = new Gasto({
-        provider_id:    req.user.provider_id,
-        descripcion:     req.body.descripcion,
-        fecha:  req.body.fecha,
-        categoria:   req.body.categoria,
-        importe:  req.body.importe
-    });
 
-    gasto.save(function(err, resultado) {
-        if(err) return res.status(500).send( err.message);
-        res.status(200).jsonp(resultado);
-    });
-}
+    let provider_id = req.user.provider_id;
+    let descripcion = req.body.descripcion;
+    let fecha = req.body.fecha;
+    let categoria = req.body.categoria;
+    let importe = req.body.importe;
+
+    if (provider_id != null && provider_id != "" && descripcion != null && descripcion != "" && fecha != null && fecha != "" && categoria != null  && categoria != "" && importe != null && importe != "") {
+        let gasto = new Gasto({
+            provider_id:    provider_id,
+            descripcion:     descripcion,
+            fecha:  fecha,
+            categoria:   categoria,
+            importe:  importe
+        });
+
+        gasto.save(function (err, resultado) {
+            if (err) return res.status(500).send(err.message);
+            res.status(200).jsonp(resultado);
+        });
+    }else{
+        res.status(201).jsonp({error: "Todos los campos son obligatorios"});
+    }
+};
--- public/javascripts/code.js
+++ public/javascripts/code.js
@@ -1,4 +1,62 @@
+class Gasto{
+     constructor(descripcion, categoria, fecha, importe) {
+        this.descripcion = descripcion;
+        this.categoria = categoria;
+        this.fecha = fecha;
+        this.importe = importe;
+    }
+}
+
+class VistaGasto{
+    static printGasto(gasto){
+        let div_cont = $("<div>");
+        div_cont.addClass("row cont-gasto");
+        let div_icono = $("<div>");
+        div_icono.addClass("columna-icono col-xs-12 col-md-3");
+        let div_cuerpo = $("<div>");
+        div_cuerpo.addClass("columna-cuerpo col-xs-12 col-md-9");
+        div_cuerpo.html(gasto.descripcion);
+        div_cont.append(div_icono);
+        div_cont.append(div_cuerpo);
+
+        return div_cont;
+    }
+}
+
 $(document).ready(function(){
-    // code here
-    
+    $("#b-guardar").click(function(){
+        // cogemos los datos y los validamos
+        let descripcion = $("#desc-gasto").val();
+        let categoria = $("#cat-gasto").val();
+        let fecha = $("#fecha-gasto").val();
+        let importe = $("#importe-gasto").val();
+
+        $.ajax({
+            method: "POST",
+            url: "/api/gastos",
+            data: {
+                descripcion: descripcion,
+                categoria: categoria,
+                fecha: fecha,
+                importe: importe
+            },
+            statusCode: {
+                404: function(){
+                    alert("Página no encontrada");
+                },
+                201: function(msg){
+                    $("#titulo-modal").html("ERROR");
+                    $("#cuerpo-modal").html("<p>"+msg.error+"</p>");
+                    $("#myModal").modal();
+                },
+                200: function(msg){
+                    $("#titulo-modal").html("¡ENHORABUENA! Tu gasto se ha podido guardar correctamente");
+                    gasto = new Gasto(msg.descripcion, msg.categoria, msg.fecha, msg.importe);
+
+                    $("#cuerpo-modal").append(VistaGasto.printGasto(gasto));
+                    $("#myModal").modal();
+                }
+            }
+        });
+    });
 });
